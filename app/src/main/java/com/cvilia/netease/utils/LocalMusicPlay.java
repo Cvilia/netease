2b11cb019cc297a47c96fd2a27dc7d73baffaa32
cvilia <cvilia@163.com>
2021-03-09 08:58:26 +0800

    创建播放器单例

--- app/src/main/java/com/cvilia/netease/utils/LocalMusicPlay.java
+++ app/src/main/java/com/cvilia/netease/utils/LocalMusicPlay.java
@@ -2,6 +2,10 @@ package com.cvilia.netease.utils;
 
 import android.media.MediaPlayer;
 
+import com.cvilia.netease.sqlmodel.LocalMusic;
+
+import java.io.IOException;
+
 /**
  * created by: cvilia
  * e-mail: cvilia@163.com
@@ -9,4 +13,27 @@ import android.media.MediaPlayer;
  * describe:
  */
 public class LocalMusicPlay extends MediaPlayer {
+
+    private MediaPlayer player;
+
+    private static class SinglePlayer {
+        private static LocalMusicPlay instance = new LocalMusicPlay();
+    }
+
+    private LocalMusicPlay() {
+        player = new MediaPlayer();
+    }
+
+    public static LocalMusicPlay getInstance(LocalMusic music) {
+        return SinglePlayer.instance;
+    }
+
+    public void setDataSource(String path) {
+        try {
+            player.setDataSource(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
